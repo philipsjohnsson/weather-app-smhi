9c7d00180fed2a65dd209cad909b3bff439f80a8
Philip Jonsson <pj222qg@student.lnu.se>
2023-05-08 11:00:00 +0200

    init project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+// import { useRef, useState } from 'react'
+// import useFetch from '../../hooks/useFetch'
+
+const Home = () => {
+
+  return (
+      <div>
+        test
+      </div>
+    )
+}
+
+export default Home;
